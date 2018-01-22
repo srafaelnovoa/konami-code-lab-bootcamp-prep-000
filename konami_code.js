3ddf09809c37903bb7dd53c4ac0971ed029233ae
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-22 18:59:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function -test2
   const input = document.querySelector('body')
+  input.addEventListener('keydown', onKeyDownHandler(e))
   //input.addEventListener('keydown', function(e) {
   //console.log(e.which)
 //})
@@ -10,6 +11,8 @@ function init() {
 }
 
 function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log(e)
   
 }
 
