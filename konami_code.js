bdd100224b38510011ef6be1d5a79e30a9cb5a63
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-22 18:55:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  // Write your JavaScript code inside the init() function -test2
+  const input = document.querySelector('body')
+  input.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
 }
+
+init()
