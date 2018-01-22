8a39ce8fe54ea34534a5828a7a906ad3afc86c22
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-22 18:59:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function -test2
   const input = document.querySelector('body')
-  input.addEventListener('keydown', onKeyDownHandler(e))
+  input.addEventListener('keydown', onKeyDownHandler)
   //input.addEventListener('keydown', function(e) {
   //console.log(e.which)
 //})
